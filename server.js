535a0e50013458e6760164632a49a670776071e5
Bahaa Al juboori <bahaaljuboori.Bahaali273@gmail.com>
2023-05-31 17:08:04 -0700

    init

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT;
+const cors = require("cors");
+const videosRoutes = require("./routes/videos");
+
+app.use(express.static("public/images")); // allows access to public folder
+app.use(express.json()); // parses req.body
+app.use(cors());
+
+app.use("/videos", videosRoutes);
+
+app.listen(port, () => {
+  console.log(`app running on port ${port}`);
+});
